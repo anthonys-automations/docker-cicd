b17ad7b987ef4c7d7086c8935e8de4bd9a0f3b6f
Anthony Verevkin <anthony@verevkin.ca>
2024-12-09 14:48:27 -0500

    github-runner

---
+++ github-runner/docker-build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -z "${REGISTRY_PASSWORD}" ] ; then
+  echo "REGISTRY_PASSWORD not set"
+  exit 255
+fi
+
+REGISTRY='registry.oci.verevkin.ca:5000'
+IMAGE_NAME='infrastructure/github-runner'
+
+# Exit on errors
+set -e
+
+# Clean up old login session
+#rm /root/.docker/config.json || true
+
+docker login -u github --password-stdin ${REGISTRY} << EOF
+${REGISTRY_PASSWORD}
+EOF
+
+docker image build . --pull --no-cache --force-rm --tag ${REGISTRY}/${IMAGE_NAME}
+docker image push ${REGISTRY}/${IMAGE_NAME}
+docker image prune -a -f
