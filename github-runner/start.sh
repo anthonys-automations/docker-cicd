afe781ebb2a5b27cfb5b318e8e457b8c20306390
Anthony Verevkin <anthony@verevkin.ca>
2024-12-15 20:20:37 -0500

    updates

--- github-runner/start.sh
+++ github-runner/start.sh
@@ -3,8 +3,6 @@
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
-
 # Path to the Docker socket
 DOCKER_SOCK="/var/run/docker.sock"
 
@@ -15,21 +13,23 @@ if [ -S "${DOCKER_SOCK}" ]; then
     
     if [ -n "${DOCKER_GID}" ]; then
         groupmod -g "${DOCKER_GID}" docker
-        usermod -aG docker github
+        usermod -aG docker ${USER}
     fi
 fi
 
+REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+
 cd /opt/actions-runner
-sudo -u github /opt/actions-runner/config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+sudo -u ${USER} /opt/actions-runner/config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
 
 cleanup() {
     echo "Refreshing registration token..."
     REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
     echo "Removing runner..."
-    sudo -u github /opt/actions-runner/config.sh remove --unattended --token ${REG_TOKEN}
+    sudo -u ${USER} /opt/actions-runner/config.sh remove --unattended --token ${REG_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
-sudo -u github /opt/actions-runner/run.sh & wait $!
+sudo -u ${USER} /opt/actions-runner/run.sh & wait $!
